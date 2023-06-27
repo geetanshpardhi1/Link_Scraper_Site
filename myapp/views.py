3b1c3e1d89cafe3b7af9d31aa5a4aecbee7fce25
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2023-06-27 17:31:29 +0530

    created link scraper

---
+++ myapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from .models import Scrap_link
+from  django.http import HttpResponseRedirect
+
+# Create your views here.
+def scrape(request):
+    if request.method == 'POST':
+        site = request.POST.get('site',"")
+
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text,'html.parser')
+   
+        for link in soup.find_all('a'):
+            link_address = link.get('href')
+            link_text = link.string
+            Scrap_link.objects.create(address=link_address,name=link_text)
+        return HttpResponseRedirect('/')
+    else:
+        data = Scrap_link.objects.all()  
+    
+   
+            
+    return render(request,'myapp/result.html',{'link':data})
+
+def clear(request):
+    Scrap_link.objects.all().delete()
+    return render(request,'myapp/result.html')
+    
